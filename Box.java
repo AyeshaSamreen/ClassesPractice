bb3ee26d6fa7d5ab9d04b28c78caa92d05f7e826
mehwishshabbir <mehwish.shabbir57@yahoo.com>
2017-03-18 07:33:16 +0500

    Adding Drived Class Box

---
+++ Box.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author shani
+ */
+public class Box {
+    private double height;
+    private double width;
+    private double depth;
+    private boolean full;//status of box
+            
+    public Box(double h,double w, double d, boolean f)
+    {
+     
+         height = h;
+         width = w;
+         depth = d;
+         full = f;
+    }
+    
+    public void setHeigth(double hi)
+    {
+        height = hi;
+    }
+    
+     public void setWidth(double wi)
+    {
+        width = wi;
+    }
+     
+      public void setDepth(double de)
+    {
+        depth = de;
+    }
+      
+       public double getHeigth()
+    {
+        return height;
+    }
+       public double getWidth()
+    {
+        return width;
+    }
+       public double getDepth()
+    {
+        return depth;
+    }
+       
+       public  void setFull(boolean b)
+       {
+           full = b;
+       }
+       public  boolean getFull()
+       {
+           
+           return full;
+       }
+       public String toString()
+       {
+           String Result ="Heigth of Box is:"+getHeigth()+"Width is:"+getWidth()+"Depth od Box is:"+getDepth()+"And Status is:"+getFull();
+           String Result1 = (getFull()==true?"  Full":"Empty");
+           return Result+= Result1;
+       }
+}
